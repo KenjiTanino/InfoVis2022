3105aff5cf1410032001b427f5cee86be961f542
kenji <ha-timituremonn@outlook.jp>
2022-05-21 21:43:20 +0900

    Initial Commit

---
+++ W08/task10-1.js
@@ -0,0 +1,107 @@
+d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W04/task2.csv")
+    .then( data => {
+        data.forEach( d => {  d.value = +d.value; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 256,
+            height: 256,
+            margin: {top:10, right:10, bottom:50, left:80}
+        };
+
+        const barchart = new Barchart( config, data );
+        barchart.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+class Barchart {
+
+    constructor( config, data ) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || {top:10, right:10, bottom:40, left:10}
+        };
+        this.data = data;
+        this.init();
+    }
+
+
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select( self.config.parent )
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+//allright
+        self.xscale = d3.scaleLinear()
+            .range( [0, self.inner_width] );
+
+        self.yscale = d3.scaleBand()
+            .range( [0, self.inner_height] )
+            .paddingInner(0.1);
+
+        self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(5)
+            .tickSizeOuter(0);
+
+        self.yaxis = d3.axisLeft( self.yscale )
+            .ticks(10);
+
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+
+        self.yaxis_group = self.chart.append('g');
+
+        self.svg.append('text')
+        .attr('x', self.config.width / 2)
+        .attr('y', self.inner_height + self.config.margin.top+30)
+        .text( "population" );
+
+        self.chart.append('g')
+            .append("text")
+            .attr("x",-40)
+            .attr("y",(this.inner_height)/2)
+            .attr("transform","rotate(-90,-40,108)")
+            .attr("text-anchor","middle")
+            .attr("font-size","20")
+            .text("Y-label")
+            .text( "prefecture" );
+    }
+
+    update() {
+        let self = this;
+
+        const xmax = d3.max( self.data, d => d.value );
+        self.xscale.domain( [0, xmax] );
+
+        self.yscale.domain(self.data.map(d => d.label));
+
+        self.render();
+
+        update(this.data);
+    }
+
+    render() {
+        let self = this;
+
+        update(this.data);
+
+
+        self.xaxis_group
+            .call( self.xaxis );
+
+        self.yaxis_group
+            .call( self.yaxis );
+}
+}
---
+++ W10/task10-1-2.js
@@ -0,0 +1,109 @@
+d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W04/task2.csv")
+    .then( data => {
+        data.forEach( d => {  d.value = +d.value; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 356,
+            height: 256,
+            margin: {top:10, right:10, bottom:50, left:80}
+        };
+
+        const barchart = new Barchart( config, data );
+        barchart.update();
+
+        d3.select('#reverse')
+        .on('click', d => {
+            data.reverse();
+            barchart.update();
+        });
+    
+    d3.select('#ascend')
+        .on('click', d => {
+              data.sort((a, b) => a.value - b.value)
+              barchart.update();
+        });
+
+    d3.select('#descend')
+        .on('click', d => {
+              data.sort((a, b) => b.value - a.value)
+              barchart.update();
+    });
+
+})
+    .catch( error => {
+        console.log( error );
+    });
+
+class Barchart {
+
+    constructor( config, data ) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || {top:10, right:10, bottom:40, left:10}
+        };
+        this.data = data;
+        this.init();
+    }
+
+
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select( self.config.parent )
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+//allright
+        self.xscale = d3.scaleLinear()
+            .range( [0, self.inner_width] );
+
+        self.yscale = d3.scaleBand()
+            .range( [0, self.inner_height] )
+            .paddingInner(0.1);
+
+        self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(5)
+            .tickSizeOuter(0);
+
+        self.yaxis = d3.axisLeft( self.yscale )
+            .ticks(10);
+
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+
+        self.yaxis_group = self.chart.append('g');
+
+        
+    }
+
+    update() {
+        let self = this;
+
+        const xmax = d3.max( self.data, d => d.value );
+        self.xscale.domain( [0, xmax] );
+
+        self.yscale.domain(self.data.map(d => d.label));
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+
+        self.xaxis_group
+            .call( self.xaxis );
+
+        self.yaxis_group
+            .call( self.yaxis );
+}
+}
